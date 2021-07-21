d37f0c4d0ad4f7bc0f999675d1afaa8f8f4561a7
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-07-21 05:53:26 +0530

    angles of the triangle

---
+++ anglesOfTriangle.js
@@ -0,0 +1,27 @@
+const formTriangle = document.querySelector("#form-triangle");
+const inputAngles = document.querySelectorAll(".input-angle");
+const outputDiv = document.querySelector("#is-triangle");
+
+let angles = [];
+
+document.addEventListener("submit", formTriangleHandler);
+
+function formTriangleHandler(e) {
+  e.preventDefault();
+
+ //add the angles input
+  for (i = 0; i < inputAngles.length; i++) {
+    angles[i] = Number(inputAngles[i].value);
+  }
+
+  let sum = 0;
+  angles.map((angle) => {
+    sum = sum + angle;
+  });
+
+  if (sum === 180) {
+    outputDiv.innerText = "Yuhuaaa! these angles can make a triangle";
+  } else {
+    outputDiv.innerText = "Oopsss! these angles cannot make a triangle";
+  }
+}
